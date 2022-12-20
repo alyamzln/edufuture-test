a843d5eb9de7d6516d6dd58670f9485293e37d25
Irdina <irdinairwan1@gmail.com>
2022-12-20 11:35:21 +0800

    Test Cloud Messaging

--- app/src/main/java/com/example/edufutureapp/activities/MainActivity.java
+++ app/src/main/java/com/example/edufutureapp/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.edufutureapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,11 +8,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.edufutureapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,26 @@ public class MainActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        etToken = (findViewById(R.id.etToken));
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            System.out.println ("Fetching FCM registration token failed");
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+                        System.out.println (token);
+                        Toast.makeText(MainActivity.this, "Your device registration token is " + token, Toast.LENGTH_SHORT).show();
+                        etToken.setText(token);
+
+                    }
+                });
     }
 
     public void signup(View view) {
